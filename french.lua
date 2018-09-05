e839da7475d4d0859b626598e7b4b830053b4ab0
Parrot <42979815+ParrotDev@users.noreply.github.com>
2018-09-05 10:33:43 +0200

    Update french.lua

--- french.lua
+++ french.lua
@@ -1,1 +1,7 @@
-Here the french language
+--[[
+	Parrot's Anti Cheat - French.
+Traduction par Parrot ( STEAM_0:0:444952038 )
+]]
+
+
+Traduction Francaise pour PAC, uniquement pour les clients de gmodstore.
